9c76d2b77c80e450a17709b86451347a42c01f5a
Dude so hot <djohn@fbi.gov>
2025-11-12 15:56:05 +0800

    Use paging-compose kmp artifacts

--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/util/illust-fetch-screen.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/util/illust-fetch-screen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.paging.compose.collectAsLazyPagingItems
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import coil3.compose.AsyncImage
--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/util/paging-compose.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/util/paging-compose.kt
@@ -21,6 +21,7 @@ import androidx.paging.PagingSource.LoadResult
 import androidx.paging.PagingSource.LoadResult.Page
 import androidx.paging.PagingState
 import androidx.paging.awaitNotLoading
+import androidx.paging.compose.LazyPagingItems
 import arrow.core.identity
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
@@ -72,86 +73,6 @@ suspend inline fun <T : Any> LoadParams<Int>.page(
     val k = key ?: 1
     val r = f(k).takeUnless { it.isEmpty() } ?: return empty()
     return LoadResult.Page(r, if (k > 1) k - 1 else null, k + 1)
-}
-
-class LazyPagingItems<T : Any>(private val flow: Flow<PagingData<T>>) {
-    private val mainDispatcher = Dispatchers.Main
-
-    private val pagingDataPresenter = object : PagingDataPresenter<T>(
-        mainContext = mainDispatcher,
-        cachedPagingData = if (flow is SharedFlow<PagingData<T>>) flow.replayCache.firstOrNull() else null,
-    ) {
-        override suspend fun presentPagingDataEvent(event: PagingDataEvent<T>) {
-            updateItemSnapshotList()
-        }
-    }
-
-    var itemSnapshotList by mutableStateOf(pagingDataPresenter.snapshot())
-        private set
-
-    val itemCount: Int
-        get() = itemSnapshotList.size
-
-    private fun updateItemSnapshotList() {
-        itemSnapshotList = pagingDataPresenter.snapshot()
-    }
-
-    operator fun get(index: Int): T? {
-        pagingDataPresenter[index] // this registers the value load
-        return itemSnapshotList[index]
-    }
-
-    fun peek(index: Int): T? = itemSnapshotList[index]
-
-    fun retry() = pagingDataPresenter.retry()
-
-    var loadState: CombinedLoadStates by
-        mutableStateOf(
-            pagingDataPresenter.loadStateFlow.value
-                ?: CombinedLoadStates(
-                    refresh = InitialLoadStates.refresh,
-                    prepend = InitialLoadStates.prepend,
-                    append = InitialLoadStates.append,
-                    source = InitialLoadStates,
-                ),
-        )
-        private set
-
-    suspend fun collectLoadState() {
-        pagingDataPresenter.loadStateFlow.filterNotNull().collect { loadState = it }
-    }
-
-    suspend fun collectPagingData() {
-        flow.collectLatest { pagingDataPresenter.collectFrom(it) }
-    }
-}
-
-private val IncompleteLoadState = LoadState.NotLoading(false)
-private val InitialLoadStates = LoadStates(LoadState.Loading, IncompleteLoadState, IncompleteLoadState)
-
-@Composable
-fun <T : Any> Flow<PagingData<T>>.collectAsLazyPagingItems(
-    context: CoroutineContext = EmptyCoroutineContext,
-): LazyPagingItems<T> {
-    val lazyPagingItems = remember(this) { LazyPagingItems(this) }
-
-    LaunchedEffect(lazyPagingItems) {
-        if (context == EmptyCoroutineContext) {
-            lazyPagingItems.collectPagingData()
-        } else {
-            withContext(context) { lazyPagingItems.collectPagingData() }
-        }
-    }
-
-    LaunchedEffect(lazyPagingItems) {
-        if (context == EmptyCoroutineContext) {
-            lazyPagingItems.collectLoadState()
-        } else {
-            withContext(context) { lazyPagingItems.collectLoadState() }
-        }
-    }
-
-    return lazyPagingItems
 }
 
 suspend fun <T : Any> LazyPagingItems<T>.awaitNextState() {
