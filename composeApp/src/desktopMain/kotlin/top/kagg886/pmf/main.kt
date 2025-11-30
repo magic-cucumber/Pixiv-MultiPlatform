4d833ebed71932b8f5b38eb5c0c70d0e1ab31b25
magic-cucumber <kagg886@qq.com>
2025-11-30 08:01:44 +0800

    disallow multi-instance on desktop client (#13)

--- composeApp/src/desktopMain/kotlin/top/kagg886/pmf/main.kt
+++ composeApp/src/desktopMain/kotlin/top/kagg886/pmf/main.kt
@@ -7,13 +7,32 @@ import co.touchlab.kermit.Logger
 import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
+import java.nio.channels.FileChannel
+import java.nio.channels.FileLock
+import java.nio.file.StandardOpenOption
+import javax.swing.JOptionPane
 import kotlin.system.exitProcess
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.compose.resources.painterResource
-import top.kagg886.pmf.res.*
+import top.kagg886.pmf.backend.dataPath
+import top.kagg886.pmf.res.Res
+import top.kagg886.pmf.res.kotlin
+import top.kagg886.pmf.res.multiapp_hint
+import top.kagg886.pmf.res.multiapp_not_allowed
+import top.kagg886.pmf.res.warning
 import top.kagg886.pmf.ui.route.crash.CrashApp
 import top.kagg886.pmf.ui.route.welcome.WelcomeRoute
+import top.kagg886.pmf.util.absolutePath
+import top.kagg886.pmf.util.createNewFile
+import top.kagg886.pmf.util.exists
+import top.kagg886.pmf.util.getString
+import top.kagg886.pmf.util.mkdirs
+import top.kagg886.pmf.util.parentFile
+import top.kagg886.pmf.util.writeString
 
 fun launch(start: () -> NavKey) {
     setupEnv()
@@ -68,4 +87,36 @@ fun launch(start: () -> NavKey) {
     exitProcess(0)
 }
 
-fun main() = launch { WelcomeRoute }
+@OptIn(DelicateCoroutinesApi::class)
+fun main() {
+    val file = dataPath.resolve("pmf.lock")
+    if (!file.exists()) {
+        file.absolutePath().parentFile()?.mkdirs()
+        file.createNewFile()
+        file.writeString(runBlocking { getString(Res.string.multiapp_hint) })
+    }
+
+    val lock: FileLock? = runCatching {
+        FileChannel.open(file.toNioPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE).tryLock()
+    }.getOrNull()
+
+    if (lock == null) {
+        runBlocking(Dispatchers.Main) {
+            JOptionPane.showMessageDialog(
+                null,
+                getString(Res.string.multiapp_not_allowed),
+                getString(Res.string.warning),
+                JOptionPane.WARNING_MESSAGE,
+            )
+            exitProcess(0)
+        }
+    }
+
+    Runtime.getRuntime().addShutdownHook(
+        Thread {
+            lock.release()
+        },
+    )
+
+    launch { WelcomeRoute }
+}
