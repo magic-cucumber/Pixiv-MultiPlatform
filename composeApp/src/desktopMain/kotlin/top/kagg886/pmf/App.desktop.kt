04a383e5936b47db9a25d4d2178564e93dcf240a
magic-cucumber <kagg886@qq.com>
2025-12-05 19:11:01 +0800

    fix: 修复了macOS上复制剪贴板却被认为是文件类型的bug (#18)
    * fix: 修复了macOS上复制剪贴板却被认为是文件类型的bug
    
    * feature: iOS支持复制到剪贴板
    
    * fix
    
    * fix

--- composeApp/src/desktopMain/kotlin/top/kagg886/pmf/App.desktop.kt
+++ composeApp/src/desktopMain/kotlin/top/kagg886/pmf/App.desktop.kt
@@ -5,11 +5,14 @@ import java.awt.Desktop
 import java.awt.Toolkit
 import java.awt.datatransfer.*
 import java.io.ByteArrayInputStream
+import java.io.File
 import java.net.URI
 import javax.imageio.ImageIO
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okio.Path
+import top.kagg886.pmf.backend.Platform
+import top.kagg886.pmf.backend.currentPlatform
 import top.kagg886.pmf.util.AnimatedSkiaImageDecoder
 
 actual fun openBrowser(link: String) {
@@ -21,11 +24,56 @@ actual fun shareFile(file: Path, name: String, mime: String) {
 }
 
 actual suspend fun copyImageToClipboard(bitmap: ByteArray) {
+    if (currentPlatform is Platform.Desktop.MacOS) {
+        // 在mac上调命令行解决吧，不想写额外的动态库了
+        copyImageAsImageToClipboardOnMacOS(bitmap)
+        return
+    }
+
+    // fallback
     withContext(Dispatchers.IO) {
         Toolkit.getDefaultToolkit().systemClipboard.setContents(
             TransferableImage(bitmap),
-            DesktopClipBoardOwner,
+            null,
         )
+    }
+}
+
+private suspend fun copyImageAsImageToClipboardOnMacOS(imageBytes: ByteArray) {
+    withContext(Dispatchers.IO) {
+        val pngFile = File.createTempFile("clipboard_img_", ".png")
+        pngFile.writeBytes(imageBytes)
+
+        val tiffFile = File.createTempFile("clipboard_img_", ".tiff")
+        if (tiffFile.exists()) tiffFile.delete()
+
+        val sips = ProcessBuilder("sips", "-s", "format", "tiff", pngFile.absolutePath, "--out", tiffFile.absolutePath)
+            .redirectErrorStream(true)
+            .start()
+        val sipsExit = sips.waitFor()
+        if (sipsExit != 0) {
+            // 清理并抛出或记录错误
+            pngFile.delete()
+            tiffFile.delete()
+            throw RuntimeException("sips transferred failed，exit=$sipsExit")
+        }
+
+        val appleScriptCmd = listOf(
+            "osascript",
+            "-e",
+            "set the clipboard to (read (POSIX file \"${tiffFile.absolutePath}\") as TIFF picture)",
+        )
+        val osascript = ProcessBuilder(appleScriptCmd)
+            .redirectErrorStream(true)
+            .start()
+        val osExit = osascript.waitFor()
+        if (osExit != 0) {
+            pngFile.delete()
+            tiffFile.delete()
+            throw RuntimeException("osascript execute failed，exit=$osExit")
+        }
+        pngFile.delete()
+        tiffFile.delete()
     }
 }
 
--- composeApp/src/iosMain/kotlin/top/kagg886/pmf/App.ios.kt
+++ composeApp/src/iosMain/kotlin/top/kagg886/pmf/App.ios.kt
@@ -1,13 +1,20 @@
 package top.kagg886.pmf
 
 import coil3.ComponentRegistry
+import korlibs.memory.startAddressOf
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.usePinned
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okio.Path
+import platform.Foundation.NSData
 import platform.Foundation.NSURL
+import platform.Foundation.dataWithBytes
 import platform.UIKit.UIActivityViewController
 import platform.UIKit.UIApplication
+import platform.UIKit.UIImage
+import platform.UIKit.UIPasteboard
 import top.kagg886.pmf.util.AnimatedSkiaImageDecoder
 import top.kagg886.pmf.util.absolutePath
 
@@ -37,5 +44,19 @@ actual fun shareFile(file: Path, name: String, mime: String) {
     }
 }
 
-actual suspend fun copyImageToClipboard(bitmap: ByteArray): Unit = throw UnsupportedOperationException()
+@OptIn(ExperimentalForeignApi::class)
+actual suspend fun copyImageToClipboard(bitmap: ByteArray) {
+    val board = UIPasteboard.generalPasteboard
+
+    val image = bitmap.usePinned {
+        UIImage.imageWithData(
+            data = NSData.dataWithBytes(
+                bytes = it.startAddressOf,
+                length = bitmap.size.toULong(),
+            ),
+        )
+    }
+
+    board.image = image
+}
 actual fun ComponentRegistry.Builder.installGifDecoder() = add(AnimatedSkiaImageDecoder.Factory)
