91ec56c7b236567fa6642dc0f15f7a23eb6b10c8
magic-cucumber <kagg886@qq.com>
2025-12-17 15:40:11 +0800

    fix: oom

--- composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/util/illust-fetch-screen.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/pmf/ui/util/illust-fetch-screen.kt
@@ -115,11 +115,11 @@ private fun IllustFetchContent0(state: IllustFetchViewState, model: IllustFetchV
                             Card(
                                 modifier = Modifier.fillMaxSize(),
                                 onClick = {
-                                    val startIndex = i.coerceIn(0, i - 5)
-                                    val endIndex = i.coerceIn(i + 5, data.itemCount - 1)
+                                    val startIndex = i.coerceIn(0, i - 10)
+                                    val endIndex = i.coerceIn(i + 10, data.itemCount - 1)
                                     stack += IllustDetailRoute(
                                         i,
-                                        data.itemSnapshotList.items.subList(startIndex, endIndex)
+                                        data.itemSnapshotList.items.subList(startIndex, endIndex),
                                     )
                                 },
                             ) {
