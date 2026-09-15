8fe93bcaca28396b854f3fdd477e6b6a2c341bcb
kagg886 <kagg886@qq.com>
2026-09-15 15:01:32 +0800

    we can restore when application launched (#99)
    * we can restore when application launched
    
    * fix

---
+++ composeApp/src/desktopMain/kotlin/top/kagg886/pmf/backend/DesktopWindowConfig.kt
@@ -0,0 +1,12 @@
+package top.kagg886.pmf.backend
+
+import com.russhwolf.settings.Settings
+import com.russhwolf.settings.int
+import com.russhwolf.settings.nullableInt
+
+object DesktopWindowConfig : Settings by SystemConfig.getConfig("desktop-window") {
+    var x by nullableInt("x")
+    var y by nullableInt("y")
+    var w by int("w", 800)
+    var h by int("h", 600)
+}
